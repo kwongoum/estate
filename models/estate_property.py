fbf1856358a9060c458b8da088a7541ab1ce4d99
Avit Wongoum <avit.wongoum@convivial.be>
2026-01-28 18:49:57 +0100

    use scaffold

--- models/estate_property.py
+++ models/estate_property.py
@@ -51,6 +51,10 @@ class EstateProperty(models.Model):
 						   compute ='_compute_total_area',
 						   store=True
 						   )
+    @api.depends("living_area","garden_area")	
+	def _compute_total_area(self):
+		for prop in self: 
+			prop.total_area=(prop.living_area or 0)+(prop.garden_area or 0)
 
 	_sql_constraints = [
 		(
@@ -78,10 +82,7 @@ class EstateProperty(models.Model):
 				raise ValidationError("Selling price cannot be inferior to expected price")
 			
 
-	@api.depends("living_area","garden_area")	
-	def _compute_total_area(self):
-		for prop in self: 
-			prop.total_area=(prop.living_area or 0)+(prop.garden_area or 0)
+	
 
 	""" @api.onchange("garden")
 	def _onchange_garden(self):
