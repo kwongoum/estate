c6543c0b36ba9d286de5435537c785e089a00b3d
Avit Wongoum <avit.wongoum@convivial.be>
2026-02-23 23:16:17 +0100

    files get from branch 17.0-be

--- controllers/estate_property.py
+++ controllers/estate_property.py
@@ -2,30 +2,28 @@ import json
 import requests
 from odoo import http
 
+
 class EstateProperty(http.Controller):
-    
-    
-    @http.route('/welcome', auth='public', methods=['GET', 'POST'])
+
+    @http.route("/welcome", auth="public", methods=["GET", "POST"])
     def healthcheck(self, **kw):
         return "IT Works: message for anyone: public, user"
 
-    @http.route('/profil', auth='user')
+    @http.route("/profil", auth="user")
     def xyz(self, **kw):
         return " It's authenticated: message for authenticated user"
 
-    @http.route('/api/users', auth='user', type='json', methods=['GET'])
+    @http.route("/api/users", auth="user", type="json", methods=["GET"])
     def get_users(self, **kwargs):
-        users = request.env['res.users'].search([])
+        users = requests.env["res.users"].search([])
         result = []
         for user in users:
-            result.append({
-                'id': user.id,
-                'name': user.name,
-                'email': user.login
-            })
+            result.append({"id": user.id, "name": user.name, "email": user.login})
         return result
-    
-    @http.route('/entries', auth='public')
+
+    @http.route("/entries", auth="public")
     def entries(self, **kw):
-        response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects/100")
+        response = requests.get(
+            "https://collectionapi.metmuseum.org/public/collection/v1/objects/100"
+        )
         return json.dumps(response.json())
